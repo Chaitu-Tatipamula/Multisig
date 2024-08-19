033dcf8507d05429842bc5f6b0e9c5b581e84098
Chaitu-Tatipamula <chaitutatipamula023@gmail.com>
2024-08-19 19:59:48 +0530

    first commit

---
+++ contracts/src/Wallet.sol
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.12;
+
+import {IEntryPoint} from "account-abstraction/interfaces/IEntryPoint.sol";
+import {BaseAccount} from "account-abstraction/core/BaseAccount.sol";
+import {UserOperation} from "account-abstraction/interfaces/UserOperation.sol";
+import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import {Initializable} from "@openzeppelin/contracts/proxy/utils/Initializable.sol";
+
+contract Wallet is BaseAccount, Initializable  {
+
+    using ECDSA for bytes32;
+    address[] public owners;
+
+    address public immutable walletFactory;
+    IEntryPoint private immutable _entryPoint;
+
+    event WalletInitialized(IEntryPoint indexed entryPoint, address[] owners);
+
+    constructor(IEntryPoint anEntryPoint, address ourWalletFactory) {
+        _entryPoint = anEntryPoint;
+        walletFactory = ourWalletFactory;
+    }
+
+    function entryPoint() public view override returns (IEntryPoint) {
+        return _entryPoint;
+    }
+
+    function _validateSignature(
+        UserOperation calldata userOp,
+        bytes32 userOpHash
+    ) internal view override returns (uint256) {
+        bytes32 hash = userOpHash.toEthSignedMessageHash();
+
+        bytes[] memory signatures = abi.decode(userOp.signature, (bytes[]));
+
+        for (uint256 i = 0; i < owners.length; i++) {
+            if (owners[i] != hash.recover(signatures[i])) {
+                return SIG_VALIDATION_FAILED;
+            }
+        }
+        return 0;
+    }
+
+    function initialize(address[] memory initialOwners) public initializer {
+        _initialize(initialOwners);
+    }
+
+    function _initialize(address[] memory initialOwners) internal {
+        require(initialOwners.length > 0, "no owners");
+        owners = initialOwners;
+        emit WalletInitialized(_entryPoint, initialOwners);
+    }
+
+
+}
